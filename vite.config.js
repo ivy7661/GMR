d363957ff2c704ebeeb3ea222e379292847414f5
Ivy <ivy7661@gmail.com>
2024-05-24 20:09:27 +0800

    GMR

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,53 @@
-import { fileURLToPath, URL } from 'node:url'
+import "dotenv/config";
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+import eslintPlugin from "vite-plugin-eslint";
+
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 export default defineConfig({
+  base: "./",
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ["btton"].includes(tag)
+        }
+      }
+    }),
+    eslintPlugin({
+      include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"]
+    })
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  build: {
+    // 小於 4KB 的資源將內聯為 base64
+    assetsInlineLimit: 4096,
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "");
+        }
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/helpers/mixin.scss";'
+      }
     }
   }
-})
+});
